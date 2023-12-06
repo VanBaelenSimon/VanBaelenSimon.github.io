64ef3650c631cf2b91d32e595c038b68d43b90a9
Simon Van Baelen <99735851+VanBaelenSimon@users.noreply.github.com>
2023-12-06 17:11:03 +0100

    changes

--- scripts/scrollTop.js
+++ scripts/scrollTop.js
@@ -1,16 +1,24 @@
 const arrowUp = document.getElementById("arrow-up");
 const arrowDown = document.getElementById("arrow-down");
+const secondPage = document.getElementById("about");
+const firstPage = document.getElementById("homepage");
 arrowUp.addEventListener("click", () => {
-  window.scroll({
-    top: 0,
-    left: 0,
+  // window.scroll({
+  //   top: 0,
+  //   left: 0,
+  //   behavior: "smooth",
+  // });
+  firstPage.scrollIntoView({
     behavior: "smooth",
   });
 });
 arrowDown.addEventListener("click", () => {
-  window.scrollBy({
-    top: window.innerHeight,
-    left: 0,
+  // window.scroll({
+  //   top: secondPage,
+  //   left: 0,
+  //   behavior: "smooth",
+  // });
+  secondPage.scrollIntoView({
     behavior: "smooth",
   });
 });
---
+++ scripts/tabSwitcher.js
@@ -0,0 +1,34 @@
+function hideOtherTabs(tabList, tab, tabPrefix) {
+  for (let index = 0; index < tabList.length; index++) {
+    if (tabPrefix + tab !== tabPrefix + tabList[index]) {
+      //check to see which tabs are
+      document.getElementById(tabPrefix + tabList[index]).style.display =
+        "none";
+      document.getElementById(tabList[index]).classList.remove("active");
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let tabPrefix = "home-";
+  let tabs = ["tab1", "tab2", "tab3"];
+
+  //clear all active pages
+  for (let index = 0; index < tabs.length; index++) {
+    document.getElementById(tabPrefix + tabs[index]).style.display = "none";
+  }
+  //set default active page
+  document.getElementById("home-tab1").style.display = "block";
+
+  //check which element the click event occurs on, display to block
+  const tabLinks = document.querySelectorAll("ul.tab-links li");
+  tabLinks.forEach(function (tabLink) {
+    tabLink.addEventListener("click", function () {
+      let tabID = this.id;
+      document.getElementById(tabPrefix + tabID).style.display = "block";
+      document.getElementById(tabID).classList.add("active");
+      // hide all other tabs
+      hideOtherTabs(tabs, tabID, tabPrefix);
+    });
+  });
+});
