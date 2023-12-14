1e33b60ba6cf31db03c1fd89e58216b328c66e9b
Simon Van Baelen <99735851+VanBaelenSimon@users.noreply.github.com>
2023-12-14 13:15:27 +0100

    added preloader
    testing to see the speed of page load in non local host envirement

---
+++ scripts/preloader.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+  setTimeout(function () {
+    const loader = document.querySelector(".preloader");
+    loader.classList.add("preloader--hidden");
+    loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+    });
+  }, 1000);
+});
