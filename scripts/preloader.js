97051fa321b02c44792a8364e7b5ad8fa338a838
Simon Van Baelen <99735851+VanBaelenSimon@users.noreply.github.com>
2023-12-15 09:09:40 +0100

    Update preloader.js

--- scripts/preloader.js
+++ scripts/preloader.js
@@ -2,8 +2,8 @@ window.addEventListener("load", () => {
   setTimeout(function () {
     const loader = document.querySelector(".preloader");
     loader.classList.add("preloader--hidden");
-    loader.addEventListener("transitionend", () => {
-      document.body.removeChild(loader);
-    });
+    //loader.addEventListener("transitionend", () => {
+    //  document.body.removeChild(loader);
+    //});
   }, 1000);
 });
